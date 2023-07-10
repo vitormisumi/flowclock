5723c8c93b38f25108202613f0df619263e2b468
Vitor Misumi <vitormisumi@gmail.com>
2023-07-10 16:46:46 -0300

    feat: flow button styling

--- src/routes/flow/+page.svelte
+++ src/routes/flow/+page.svelte
@@ -8,7 +8,7 @@
 	<Task />
 	<Clock />
 	<div class="grid justify-items-center gap-4">
-		<Button class="bg-accent-500 text-3xl shadow-lg w-48 h-16">Break</Button>
-		<Button size="xs" class="bg-secondary-100 text-secondary-900 shadow-md">Distraction</Button>
+		<Button class="bg-accent-500 hover:bg-accent-600 focus:ring-accent-300 text-3xl shadow-lg w-48 h-16">Break</Button>
+		<Button size="xs" class="bg-secondary-100 hover:bg-secondary-300 focus:ring-secondary-50 text-secondary-900 shadow-md">Distraction</Button>
 	</div>
 </div>
--- src/routes/flow/Task.svelte
+++ src/routes/flow/Task.svelte
@@ -5,7 +5,7 @@
 <div class="grid justify-items-center gap-1">
 	<p class="text-secondary-100">Focusing on:</p>
 	<Badge class="bg-accent-400 text-accent-100">Project X</Badge>
-	<Button class="bg-secondary-50 focus:bg-secondary-200 focus:ring-secondary-500 text-secondary-900 w-48 h-10"><Chevron>Task Y</Chevron></Button>
+	<Button class="bg-secondary-50 hover:bg-secondary-100 focus:bg-secondary-200 focus:ring-secondary-500 text-secondary-900 w-48 h-10"><Chevron>Task Y</Chevron></Button>
 	<Dropdown>
 		<DropdownItem>Task 1</DropdownItem>
 		<DropdownItem>Task 2</DropdownItem>
