8e4241db69121ca769396afae1e10811f6cb7849
Vitor Misumi <vitormisumi@gmail.com>
2023-07-19 19:25:26 -0300

    feat: styling signin and signup

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,31 +1,50 @@
 <script lang="ts">
-	import { Card, Label, Input, Button } from 'flowbite-svelte';
+	import { Label, Input, Button } from 'flowbite-svelte';
 	import { enhance } from '$app/forms';
 
 	export let form;
 </script>
 
 <div class="grid justify-center bg-secondary-900 h-screen items-center">
-	<Card class="grid justify-center m-4">
-		<h1 class="text-center text-xl text-primary-600 font-bold mb-4">Sign In</h1>
-		<form method="post" use:enhance>
-			<Label for="email" class="block mb-2">Email</Label>
-			<Input name="email" type="email" value={form?.email ?? ''} placeholder="Type your email">
-				<i class="fa-solid fa-envelope" aria-hidden="true" slot="left" /></Input
-			>
-			<Label for="password" class="block mt-4 mb-2">Password</Label>
-			<Input name="password" type="password" placeholder="Type your password">
-				<i class="fa-solid fa-lock" aria-hidden="true" slot="left" /></Input
-			>
-			<Button class="mt-4" type="submit">Sign In</Button>
-		</form>
-		<p class="mt-2">
-			Don't have an account yet? <span class="text-accent-500"
-				><a href="/signup">Sign up here</a></span
-			>
+	<div class="w-screen max-w-lg p-4 grid gap-8">
+		<h1 class="text-center text-xl text-primary-600 font-bold">Sign In</h1>
+		<p class="text-secondary-50">
+			New to Flouu? <a href="/signup" class="text-accent-500">Sign up here</a>.
 		</p>
-		{#if form?.message}
-			{form.message}
-		{/if}
-	</Card>
+		<form method="post" class="grid gap-4" use:enhance>
+			<div>
+				<Label for="email" class="text-primary-50 mb-2">Email</Label>
+				<Input
+					class="bg-primary-900 text-secondary-50 placeholder:text-secondary-500"
+					name="email"
+					type="email"
+					value={form?.email ?? ''}
+					placeholder="Type your email"
+				>
+					<i class="fa-solid fa-envelope" aria-hidden="true" slot="left" /></Input
+				>
+			</div>
+			<div>
+				<Label for="password" class="mb-2 text-primary-50">Password</Label>
+				<Input
+					class="bg-primary-900 text-secondary-50 placeholder:text-secondary-500"
+					name="password"
+					type="password"
+					placeholder="Type your password"
+				>
+					<i class="fa-solid fa-lock" aria-hidden="true" slot="left" /></Input
+				>
+			</div>
+			<Button class="w-full" type="submit">Sign In</Button>
+		</form>
+		<hr class="border border-y-1 border-secondary-800" />
+		<div class="grid gap-4">
+			<Button outline class="bg-secondary-50"
+				><i class="fa-brands fa-github pr-2" />Sign in with Github</Button
+			>
+			<Button outline class="bg-secondary-50"
+				><i class="fa-brands fa-google pr-2" />Sign in with Google</Button
+			>
+		</div>
+	</div>
 </div>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,17 +1,66 @@
 <!-- // src/routes/login/+page.svelte -->
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { Label, Input, Button } from 'flowbite-svelte';
+
 	export let form;
 </script>
 
-<form method="post" use:enhance>
-	<label for="email">Email</label>
-	<input name="email" value={form?.email ?? ''}>
-	<label for="password">Password</label>
-	<input type="password" name="password" />
-	<button>Sign up</button>
-</form>
-
-{#if form?.message}
-	{form.message}
-{/if}
+<div class="grid justify-center bg-secondary-900 h-screen items-center">
+	<div class="w-screen max-w-lg p-4 grid gap-8">
+		<h1 class="text-center text-xl text-primary-600 font-bold">Sign Up</h1>
+		<p class="text-primary-50">
+			Already have an account? <a href="/signin" class="text-accent-500">Sign in</a>.
+		</p>
+		<form method="post" class="grid gap-4" use:enhance>
+			<div>
+				<Label for="email" class="text-primary-50 mb-2">Email</Label>
+				<Input
+					class="bg-primary-900 text-secondary-50 placeholder:text-secondary-500"
+					name="email"
+					type="email"
+					value={form?.email ?? ''}
+					placeholder="Type your email"
+				>
+					<i
+						class="fa-solid fa-envelope text-secondary-500"
+						aria-hidden="true"
+						slot="left"
+					/></Input
+				>
+			</div>
+			<div>
+				<Label for="password" class="text-primary-50 mb-2">Password</Label>
+				<Input
+					class="bg-primary-900 text-secondary-50 placeholder:text-secondary-500"
+					name="password"
+					type="password"
+					placeholder="Create a password"
+				>
+					<i class="fa-solid fa-lock text-secondary-500" aria-hidden="true" slot="left" /></Input
+				>
+			</div>
+			<div>
+				<Label for="password-confirmation" class="text-primary-50 mb-2">Password confirmation</Label>
+				<Input
+					class="bg-primary-900 text-secondary-50 placeholder:text-secondary-500"
+					name="password-confirmation"
+					type="password"
+					placeholder="Retype your password"
+				>
+					<i class="fa-solid fa-lock text-secondary-500" aria-hidden="true" slot="left" /></Input
+				>
+			</div>
+			<Button class="w-full" type="submit">Sign Up</Button>
+		</form>
+		<hr class="border-primary-800" />
+		<div class="grid gap-4">
+			<Button outline class="bg-secondary-50"
+				><i class="fa-brands fa-github pr-2" />Sign up with Github</Button
+			>
+			<Button outline class="bg-secondary-50"
+				><i class="fa-brands fa-google pr-2" />Sign up with Google</Button
+			>
+		</div>
+	</div>
+</div>
