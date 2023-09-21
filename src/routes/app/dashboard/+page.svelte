c4fa16ffb26d47bb3746e3ed2e7910b8824fda7c
Vitor Misumi <vitormisumi@gmail.com>
2023-09-21 18:51:10 -0300

    feat: sort on distraction plot

--- src/routes/app/dashboard/+page.svelte
+++ src/routes/app/dashboard/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
-	import type { Session } from '../types';
+	import type { Distraction, Session } from '../types';
 	import type { Writable } from 'svelte/store';
-	import { filter, filteredSessions } from './stores';
+	import { filter, filteredDistractions, filteredSessions } from './stores';
 	import { fade } from 'svelte/transition';
 	import { navigating } from '$app/stores';
 	import { session, sessionBreak } from '../session/stores';
@@ -14,6 +14,7 @@
 	export let form;
 
 	const sessions: Writable<Session[]> = getContext('sessions');
+	const distractions: Writable<Distraction[]> = getContext('distractions');
 
 	const today = new Date();
 	$: current = $filter.current ? 1 : 0;
@@ -21,6 +22,14 @@
 	$: switch ($filter.timeframe) {
 		case 'day':
 			$filteredSessions = $sessions.filter((x) => {
+				const date = new Date(x.start);
+				return (
+					date.getDate() + 1 === today.getDate() + current &&
+					date.getMonth() === today.getMonth() &&
+					date.getFullYear() === today.getFullYear()
+				);
+			});
+			$filteredDistractions = $distractions.filter((x) => {
 				const date = new Date(x.start);
 				return (
 					date.getDate() + 1 === today.getDate() + current &&
@@ -36,8 +45,21 @@
 					const beginningOfWeek = new Date().setHours(0, 0, 0, 0) - today.getDay() * 86400000;
 					return date > beginningOfWeek;
 				});
+				$filteredDistractions = $distractions.filter((x) => {
+					const date = Date.parse(x.start);
+					const beginningOfWeek = new Date().setHours(0, 0, 0, 0) - today.getDay() * 86400000;
+					return date > beginningOfWeek;
+				});
 			} else {
 				$filteredSessions = $sessions.filter((x) => {
+					const date = Date.parse(x.start);
+					const beginningOfLastWeek =
+						new Date().setHours(23, 59, 59, 999) - 604800000 - (1 + today.getDay()) * 86400000;
+					const endOfLastWeek =
+						new Date().setHours(23, 59, 59, 999) - (1 + today.getDay()) * 86400000;
+					return date > beginningOfLastWeek && date < endOfLastWeek;
+				});
+				$filteredDistractions = $distractions.filter((x) => {
 					const date = Date.parse(x.start);
 					const beginningOfLastWeek =
 						new Date().setHours(23, 59, 59, 999) - 604800000 - (1 + today.getDay()) * 86400000;
@@ -55,15 +77,27 @@
 					date.getFullYear() === today.getFullYear()
 				);
 			});
+			$filteredDistractions = $distractions.filter((x) => {
+				const date = new Date(x.start);
+				return (
+					date.getMonth() + 1 === today.getMonth() + current &&
+					date.getFullYear() === today.getFullYear()
+				);
+			});
 			break;
 		case 'year':
 			$filteredSessions = $sessions.filter((x) => {
 				const date = new Date(x.start);
 				return date.getFullYear() + 1 === today.getFullYear() + current;
 			});
+			$filteredDistractions = $distractions.filter((x) => {
+				const date = new Date(x.start);
+				return date.getFullYear() + 1 === today.getFullYear() + current;
+			});
 			break;
 		default:
 			$filteredSessions = $sessions;
+			$filteredDistractions = $distractions;
 	}
 </script>
 
@@ -76,7 +110,7 @@
 >
 	<div class="grid gap-4">
 		<Filter />
-		<div class="grid place-items-start gap-4 md:grid-cols-2">
+		<div class="grid place-items-start gap-4 lg:grid-cols-2">
 			<Sessions />
 			<Distractions />
 		</div>
--- src/routes/app/dashboard/Distractions.svelte
+++ src/routes/app/dashboard/Distractions.svelte
@@ -1,32 +1,112 @@
 <script lang="ts">
-	import type { Writable } from 'svelte/store';
-	import type { Distraction } from '../types';
-	import { getContext } from 'svelte';
-	import { Card } from 'flowbite-svelte';
+	import { Button, Card, Dropdown, Radio } from 'flowbite-svelte';
+	import { millisecondsToClock } from '$lib/functions/functions';
+	import { filteredDistractions, filteredSessions } from './stores';
 
-	const distractions: Writable<Distraction[]> = getContext('distractions');
+	$: reasonDistribution = Object.fromEntries(
+		Object.entries(
+			$filteredDistractions.reduce(
+				(distribution, item) => {
+					const reason = item.reason;
+					distribution[reason] = (distribution[reason] || 0) + 1;
+					return distribution;
+				},
+				{} as { [key: string]: number }
+			)
+		).sort((x, y) => y[1] - x[1])
+	);
 
-	let reasonDistribution = Object.fromEntries(Object.entries($distractions.reduce(
-		(distribution, item) => {
-			const reason = item.reason;
-			distribution[reason] = (distribution[reason] || 0) + 1;
-			return distribution;
-		},
-		{} as { [key: string]: number }
-	)).sort((x, y) => y[1] - x[1]));
+	$: durationDistribution = Object.fromEntries(
+		Object.entries(
+			$filteredDistractions.reduce(
+				(distribution, item) => {
+					const reason = item.reason;
+					distribution[reason] =
+						(distribution[reason] || 0) + Date.parse(item.end) - Date.parse(item.start);
+					return distribution;
+				},
+				{} as { [key: string]: number }
+			)
+		).sort((x, y) => y[1] - x[1])
+	);
 
-	$: console.log(reasonDistribution);
+	let group: string = 'frequency';
+
+	$: distribution = group === 'frequency' ? reasonDistribution : durationDistribution;
+
+	let open: boolean = false;
 </script>
 
-<Card class="border-0 bg-primary-800 min-w-full">
-	<h2 class="p-2 text-center font-semibold text-primary-50 md:text-xl">Distractions</h2>
-	<svg width="100%" height="100%" class="rounded-xl bg-primary-900 fill-primary-50 p-2">
-		{#each Object.entries(reasonDistribution) as reason, value}
+<Card class="min-w-full border-0 bg-primary-800">
+	<div class="flex items-center justify-around gap-2 p-2 text-center">
+		<div>
+			<h2 class="text-center font-semibold text-primary-50 md:text-xl">Distractions</h2>
+			<p class="text-center text-lg text-accent-500 md:text-2xl">
+				{$filteredDistractions.length}
+			</p>
+		</div>
+		<div>
+			<h2 class="font-semibold text-primary-50 md:text-xl">per Session</h2>
+			<p class="text-center text-lg text-accent-500 md:text-2xl">
+				{($filteredDistractions.length / $filteredSessions.length).toFixed(2)}
+			</p>
+		</div>
+		<Button class="h-fit grid md:flex">Sort by<i class="fa-solid fa-chevron-down md:pl-2" /></Button>
+		<Dropdown class="grid gap-2 rounded-lg bg-primary-50 p-4" bind:open>
+			<li>
+				<Radio
+					name="sort"
+					bind:group
+					value="frequency"
+					on:click={() => (open = false)}
+					class="text-primary-900">frequency</Radio
+				>
+			</li>
+			<li>
+				<Radio
+					name="sort"
+					bind:group
+					value="duration"
+					on:click={() => (open = false)}
+					class="text-primary-900">duration</Radio
+				>
+			</li>
+		</Dropdown>
+	</div>
+	<svg
+		width="100%"
+		height={(Object.keys(distribution).length + 1) * 50}
+		class="rounded-xl bg-primary-900 p-2"
+	>
+		{#each Object.entries(distribution) as reason, value}
 			{#if value < 10}
-				<text x="48%" y={value * 50 + 30} text-anchor="end" class="capitalize">{reason[0]}</text>
-				<rect x="50%" y={value * 50 + 5} height="40" width="{reason[1] / $distractions.length * 50}%" />
+				<text x="48%" y={value * 50 + 30} text-anchor="end" class="fill-primary-50 capitalize"
+					>{reason[0]}</text
+				>
+				<rect
+					x="50%"
+					y={value * 50 + 5}
+					height="40"
+					width="{(reason[1] /
+						Object.values(distribution).reduce((accumulator, value) => {
+							return accumulator + value;
+						}, 0)) *
+						50}%"
+					class="fill-primary-700"
+				/>
+				<text x="52%" y={value * 50 + 30} class="fill-primary-50 capitalize"
+					>{group === 'frequency' ? reason[1] : millisecondsToClock(reason[1])}</text
+				>
 			{/if}
 		{/each}
-		<line x1="50%" y1="0" x2="50%" y2="100%" class="stroke-primary-800" />
+		<line
+			x1="50%"
+			y1="0"
+			x2="50%"
+			y2={Object.keys(distribution).length * 50}
+			class="stroke-primary-800"
+		/>
+		<text x="50%" y="98%" text-anchor="middle" class="fill-primary-50" font-weight="lighter">{group} of distractions</text
+		>
 	</svg>
 </Card>
