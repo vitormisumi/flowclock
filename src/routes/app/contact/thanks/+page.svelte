bd1902c56e118fcb0e2e9588fbfb65eb24d38bfa
Vitor Misumi <vitormisumi@gmail.com>
2023-10-02 16:22:11 -0300

    fix: minor styling

--- src/routes/app/contact/thanks/+page.svelte
+++ src/routes/app/contact/thanks/+page.svelte
@@ -1,6 +1,6 @@
 <div class="grid place-items-center content-center h-full gap-16">
 	<h2 class="text-center text-6xl text-primary-50">Thank you!</h2>
-	<p class="text-center text-lg text-primary-100">
+	<p class="text-center text-lg text-secondary-100">
 		We have received your message and will reply as soon as possible.
 	</p>
 </div>
