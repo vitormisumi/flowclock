4939827e41e24168ec6bba36b9e27d0f1a2b3f52
Vitor Misumi <vitormisumi@gmail.com>
2023-10-06 15:21:20 -0300

    fix: simpler home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,7 @@
 <script lang="ts">
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button } from 'flowbite-svelte';
 	import Hero from './Hero.svelte';
-	import { onMount } from 'svelte';
-	import { paint } from './gradient';
-
-	onMount(() => {
-		const canvas: HTMLCanvasElement | null = document.querySelector('canvas');
-		if (!canvas) {
-			throw new Error('Canvas element not found');
-		}
-		const context: CanvasRenderingContext2D | null = canvas.getContext('2d');
-		if (!context) {
-			throw new Error('2D context not available');
-		}
-
-		let frame = requestAnimationFrame(function loop(t) {
-			frame = requestAnimationFrame(loop);
-			paint(context, t);
-		});
-
-		return () => {
-			cancelAnimationFrame(frame);
-		};
-	});
+	import logo from '$lib/assets/logo.png'
 </script>
 
 <div class="flex h-screen flex-col">
@@ -32,7 +11,7 @@
 		class="sticky top-0 z-50 bg-gradient-to-b from-primary-800 to-secondary-900"
 	>
 		<NavBrand href="/">
-			<canvas class="h-8 w-40 md:w-48 lg:w-60" />
+			<img src={logo} alt="FlowClock logo" class="w-28 md:w-32 lg:w-40">
 		</NavBrand>
 		<NavHamburger on:click={toggle} menuClass="text-primary-50" />
 		<NavUl
@@ -55,12 +34,3 @@
 		<Hero />
 	</div>
 </div>
-
-<style>
-	canvas {
-		mask: url(./logo.svg) 50% 50% no-repeat;
-		mask-size: 100%;
-		-webkit-mask: url(./logo.svg) 50% 50% no-repeat;
-		-webkit-mask-size: 100%;
-	}
-</style>
--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,45 +1,9 @@
 <script lang="ts">
 	import { Button } from 'flowbite-svelte';
-
-	interface HandSettings {
-		x1: number;
-		x2: number;
-		y1: number;
-		y2: number;
-		width: number;
-		animation: string;
-	}
-
-	let hands: { [key: string]: HandSettings } = {
-		hours: {
-			x1: 50,
-			x2: 40,
-			y1: 50,
-			y2: 35,
-			width: 10,
-			animation: 'animate-hours-hand'
-		},
-		minutes: {
-			x1: 50,
-			x2: 75,
-			y1: 50,
-			y2: 15,
-			width: 5,
-			animation: 'animate-minutes-hand'
-		},
-		seconds: {
-			x1: 50,
-			x2: 75,
-			y1: 50,
-			y2: 15,
-			width: 1,
-			animation: 'animate-seconds-hand'
-		}
-	};
 </script>
 
 <section
-	class="bg-gradient-radial relative grid aspect-square place-items-center content-center gap-8 overflow-hidden from-primary-800 to-secondary-900 to-70% p-4 landscape:to-60%"
+	class="bg-gradient-radial relative grid aspect-square place-items-center content-center gap-8 overflow-hidden from-primary-800 to-secondary-900 to-75% p-4"
 >
 	<h1
 		class="z-10 text-center text-2xl font-semibold tracking-wider text-secondary-100 md:text-6xl lg:text-7xl"
@@ -54,21 +18,4 @@
 	<Button size="xl" href="/signup" class="z-50 bg-accent-500 transition-colors hover:bg-accent-600"
 		>Sign Up For FREE</Button
 	>
-	{#each Object.values(hands) as hand}
-		<svg
-			width="100%"
-			height="100%"
-			class="{hand.animation} pointer-events-none absolute p-4 md:p-8 lg:p-12"
-		>
-			<line
-				x1="{hand.x1}%"
-				x2="{hand.x2}%"
-				y1="{hand.y1}%"
-				y2="{hand.y2}%"
-				stroke-width={hand.width}
-				stroke-linecap="round"
-				class="stroke-primary-800"
-			></line>
-		</svg>
-	{/each}
 </section>
