a0999c911e2ac27b445cdcb517a712a4b809ea0d
Vitor Misumi <vitormisumi@gmail.com>
2024-01-10 11:26:28 -0300

    test: dnd on mobile 4

--- src/routes/app/projects/longpress.ts
+++ src/routes/app/projects/longpress.ts
@@ -4,40 +4,42 @@ export function longpress(node: HTMLElement, delay = 500) {
 
     function handlePointerDown(e: PointerEvent) {
         if (e.pointerType === 'mouse') {
-            node.dispatchEvent(new CustomEvent('longpress'))
-        } else if (e.pointerType === 'touch') {
-            window.addEventListener('pointermove', handleEndBeforeLong);
+            node.dispatchEvent(new CustomEvent('longpress'));
+        }
+    }
+
+    function handleTouchStart(e: TouchEvent) {
+        if (!dragging) {
+            window.addEventListener('touchmove', handleEndBeforeLong);
             timeout = window.setTimeout(() => {
-                if (!dragging) {
-                    console.log('touch long')
-                    node.dispatchEvent(new CustomEvent('longpress'));
-                    dragging = true;
-                }
-                e.preventDefault();
-                const event = new MouseEvent('mousedown', {
-                    clientX: e.clientX,
-                    clientY: e.clientY
-                })
-                window.setTimeout(() => node.dispatchEvent(event), 0);
+                window.removeEventListener('touchmove', handleEndBeforeLong)
+                dragging = true;
+                node.dispatchEvent(new CustomEvent('longpress'));
+                window.setTimeout(() => node.dispatchEvent(e), 0);
             }, delay);
         }
         e.stopPropagation();
     }
-
-    function handleEndBeforeLong(e: PointerEvent) {
+    
+    function handleEndBeforeLong(e: TouchEvent) {
         if (!dragging) {
-            console.log('before')
-            window.clearTimeout(timeout);
-            window.removeEventListener('pointermove', handleEndBeforeLong);
             dragging = false;
+            window.clearTimeout(timeout);
+            window.removeEventListener('touchmove', handleEndBeforeLong);
         }
     }
     
     function handlePointerUp(e: PointerEvent) {
+        if (e.pointerType === 'mouse') {
+            dragging = false;
+            e.stopPropagation();
+        }
+    }
+
+    function handleTouchEnd(e: TouchEvent) {
         dragging = false;
-        console.log('up')
         window.clearTimeout(timeout);
-        window.removeEventListener('pointermove', handleEndBeforeLong);
+        window.removeEventListener('touchmove', handleEndBeforeLong);
         e.stopPropagation();
     }
 
@@ -45,14 +47,25 @@ export function longpress(node: HTMLElement, delay = 500) {
         e.preventDefault();
     }
 
+    function handleTouchMove(e: TouchEvent) {
+        if (dragging) {
+            e.preventDefault();
+        }
+    }
+
     node.addEventListener('pointerdown', handlePointerDown);
     node.addEventListener('pointerup', handlePointerUp);
+    node.addEventListener('touchstart', handleTouchStart);
+    node.addEventListener('touchend', handleTouchEnd);
+    node.addEventListener('touchmove', handleTouchMove);
     node.addEventListener('contextmenu', handleContextMenu)
 
     return {
         destroy() {
             node.removeEventListener('pointerdown', handlePointerDown);
             node.removeEventListener('pointerup', handlePointerUp);
+            node.removeEventListener('touchstart', handleTouchStart);
+            node.removeEventListener('touchend', handleTouchEnd);
             node.removeEventListener('contextmenu', handleContextMenu)
         }
     };
