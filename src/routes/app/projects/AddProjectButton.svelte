535f7b7c42a472d65db11d265c642d6aac5381ce
Vitor Misumi <vitormisumi@gmail.com>
2024-02-06 15:45:58 -0300

    fix: add project modal styling

--- src/routes/app/projects/AddProjectButton.svelte
+++ src/routes/app/projects/AddProjectButton.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import { Input, Select, Textarea } from 'flowbite-svelte';
 	import { enhance } from '$app/forms';
-	import { getContext } from 'svelte';
 	import Button from '$lib/components/Button.svelte';
 	import Modal from '$lib/components/Modal.svelte';
-	import type { Writable } from 'svelte/store';
 	import type { SubmitFunction } from '@sveltejs/kit';
+	import { Input, Select, Textarea } from 'flowbite-svelte';
+	import { getContext } from 'svelte';
+	import type { Writable } from 'svelte/store';
 
 	export let group: ProjectGroup;
 
 	const projectGroups: Writable<ProjectGroup[]> = getContext('projectGroups');
 
-	let open = false;
+	let open = true;
 
 	let statusOptions = [
 		{ name: 'idea', value: 'idea' },
@@ -59,19 +59,21 @@
 		<Input
 			name="name"
 			placeholder="Project name"
-			class="border-0 bg-transparent text-xl dark:bg-transparent dark:text-secondary-50 placeholder:dark:text-secondary-500"
+			class="border-0 bg-transparent text-lg dark:bg-transparent dark:text-secondary-50 placeholder:dark:text-secondary-500"
 			required
 		></Input>
-		<Input
-			name="goal"
-			placeholder="Project goal"
-			class="border-0 bg-transparent dark:bg-transparent dark:text-secondary-200 placeholder:dark:text-secondary-500"
-		></Input>
-		<Textarea
-			name="description"
-			placeholder="Description"
-			class="border-0 bg-transparent dark:bg-transparent dark:text-secondary-200 placeholder:dark:text-secondary-500"
-		></Textarea>
+		<div class="flex place-items-start">
+			<Input
+				name="goal"
+				placeholder="Project goal"
+				class="border-0 bg-transparent dark:bg-transparent dark:text-secondary-200 placeholder:dark:text-secondary-500"
+			></Input>
+			<Textarea
+				name="description"
+				placeholder="Description"
+				class="border-0 bg-transparent dark:bg-transparent dark:text-secondary-200 placeholder:dark:text-secondary-500"
+			></Textarea>
+		</div>
 		<Select
 			items={statusOptions}
 			name="status"
@@ -89,6 +91,7 @@
 		<Button
 			type="submit"
 			buttonStyle="accent"
+			class="w-fit self-center"
 			disabled={loading}
 			on:click
 		>
