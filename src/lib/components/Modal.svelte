eb17097c3f4722b732f8b535cfdc0d9187a23221
Vitor Misumi <vitormisumi@gmail.com>
2024-02-07 16:30:28 -0300

    fix: modal border

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -5,7 +5,7 @@
 	export let open: boolean = false;
 
 	$: combinedClasses = twMerge(
-		'max-h-96 bg-secondary-50 text-center dark:bg-secondary-900 landscape:left-8 landscape:md:left-12 border-none dark:border-none',
+		'max-h-96 bg-secondary-50 text-center dark:bg-secondary-900 landscape:left-8 landscape:md:left-12 border-none dark:border-none hover:border-none hover:dark:border-none',
 		$$props.class
 	);
 </script>
