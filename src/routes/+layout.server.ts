8398637cc0aee62376e4eebeef3887377152d22f
Vitor Misumi <vitormisumi@gmail.com>
2024-04-15 17:10:49 -0300

    feat: auto sign in

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,5 @@
-import { redirect } from '@sveltejs/kit';
-
 export const load = async ({ locals: { getSession } }) => {
-	const session = await getSession();
-	if (session) redirect(303, '/app');
-
 	return {
-		session
+		session: await getSession()
 	};
 };
